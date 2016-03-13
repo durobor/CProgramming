ed6f5fc2338322bb0a4e5f931037b8e75ef5dccf
durobor <peterallemann333@gmail.com>
2016-03-13 23:36:00 +0100

    EOF doesn't seem to trigger in mac os terminal

---
+++ CountBlanks.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int c,b,t,n;
+
+	while((c=getchar())!=EOF){
+		if('\0'==c) ++b;
+		if('\t'==c) ++t;
+		if('\n'==c) ++n;
+	}
+	printf("Blanks: %d",b);
+	printf("Tabs: %d",t);
+	printf("New lines: %d",n);
+}
