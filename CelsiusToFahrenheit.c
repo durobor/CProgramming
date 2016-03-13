4b6f4d679379c05bcc91a3c7559239480fb0bf0f
durobor <peterallemann333@gmail.com>
2016-03-13 11:23:32 +0100

    reversed the table form 300 to 0

--- CelsiusToFahrenheit.c
+++ CelsiusToFahrenheit.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 
 int main(){
-	float cels, fahr;
-	int lower, upper, step;
-	
-	lower = 0;
-	upper = 300;
-	step = 20;
-
+	int celsius;
 	printf("Celsius\tFahrenheit\n");
-	cels = lower;
-	while(cels<=upper){
-		fahr = cels*9./5.+32.;
-		printf("%3.0f\t%6.1f\n", cels, fahr);
-		cels = cels + step;
+	for(celsius=300;celsius>=0;celsius-=20){
+		printf("%3d\t%6.1f\n", celsius, celsius*9./5.+32.);
+
 	}
 }
