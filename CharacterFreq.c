5c39c2465e969353d38c501bb48b04779e89c84a
durobor <peterallemann333@gmail.com>
2016-03-15 22:14:03 +0100

    added two new files

--- CharacterFreq.c
+++ CharacterFreq.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-#define CHARS 52
+#define CHARS 26
+#define A_TO_A 32
 
 int main(){
-	int i, c;
+	int i, j, c;
 	int digits[10];
 	int characters[CHARS];
 
@@ -14,13 +15,31 @@ int main(){
 		characters[i]=0;
 	}
 
-	printf("%d", 'a'-'A');
-
-	while((c=getchar()!=EOF)){
+	while((c=getchar())!=EOF){
 		if('0'<=c && '9'>=c){
 			++digits[c-'0'];
 		}
-		else if('a'<=c && '9'>=c){
+		else if('A'<=c && 'Z'>=c){
+			c+=A_TO_A;
+			++characters[c-'a'];
 		}
+		else if('a'<=c && 'z'>=c){
+			++characters[c-'a'];
+		}
+	}
+	
+	for(i=0;i<10;++i){
+		printf("%c: ", '0'+i);
+		for(j=digits[i];j>0;--j){
+			printf("|");
+		}
+		printf("\n");
+	}
+	for(i=0;i<CHARS;++i){
+		printf("%c: ", 'a'+i);
+		for(j=characters[i];j>0;--j){
+			printf("|");
+		}
+		printf("\n");
 	}
 }
--- Histogram.c
+++ Histogram.c
@@ -1,4 +1,39 @@
 #include <stdio.h>
 
+#define OUT 0
+#define IN 1
+
 int main(){
+	int cCount, i, c, state;
+	int wlengths[50];
+
+	for(i=0;i<50;++i){
+		wlengths[i]=0;
+	}
+
+	state=OUT;
+	while((c=getchar())!=EOF){
+		if(' '!=c && '\n'!=c && '\t'!=c && !state){
+			state=IN;
+			cCount=0;
+			++cCount;
+		}
+		else if(' '!=c && '\n'!=c && '\t'!=c){
+			++cCount;
+		}
+		else{
+			state=OUT;
+		}
+
+		if(!state)
+			++wlengths[cCount];
+	}
+
+	for(i=1;i<50;++i){
+		printf("\nNumber of words with length %2d:	", i);
+		int j;
+		for(j=wlengths[i];j>0;--j){
+			printf("|");
+		}
+	}
 }
