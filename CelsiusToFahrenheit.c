6480c27dd2f28665a0a09e7185309dc405fdb78d
durobor <peterallemann333@gmail.com>
2016-03-15 22:17:59 +0100

    CelsiusToFahrenheit now uses a function

--- CelsiusToFahrenheit.c
+++ CelsiusToFahrenheit.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+float toFahrenheit(int cels){
+	return cels*9./5.+32;
+}
+
 int main(){
 	int celsius;
 	printf("Celsius\tFahrenheit\n");
 	for(celsius=300;celsius>=0;celsius-=20){
-		printf("%3d\t%6.1f\n", celsius, celsius*9./5.+32.);
-
+		printf("%3d\t%6.1f\n", celsius, toFahrenheit(celsius));
 	}
 }
